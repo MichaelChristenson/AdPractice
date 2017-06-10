03b96f00676b360969a83d379677ac5a276518b4
MichaelChristenson <mochristenson@gamil.com>
2017-06-10 13:32:49 -0500

    ShowResult defined twice

---
+++ NotARealSong/Assets/Ad.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Ad : MonoBehaviour {
+
+    public void ShowAd()
+    {
+        Debug.Log("Attempting to Show Ad");
+        if (Advertisement.IsReady())
+        {
+            Debug.Log("Showing Ad");
+            Advertisement.Show("", new ShowOptions() { resultCallback = HandleAdResult });
+        }   
+    }
+
+    private void HandleAdResult(ShowResult result)
+    {
+        if(result == ShowResult.Finished)
+        {
+            reward();
+        }
+        else
+        {
+            failed();
+        }
+    }
+
+    private void reward()
+    {
+        Debug.Log("Yay");
+    }
+
+    private void skip()
+    {
+        Debug.Log("Boo");
+    }
+
+    private void failed()
+    {
+        Debug.Log("Failure");
+    }
+        
+}
